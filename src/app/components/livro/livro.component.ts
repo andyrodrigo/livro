ee14d7b388015a179f0e6b1444f76d1b93f64566
perkles <andyrodrigo2@gmail.com>
2024-06-01 11:39:48 -0300

    ajuste endereço de imagens 2

--- src/app/components/livro/livro.component.ts
+++ src/app/components/livro/livro.component.ts
@@ -22,7 +22,7 @@ export class LivroComponent implements OnInit {
     'Valéria é uma assassina impiedosa, mas, por trás de toda raiva, esconde um passado tenebroso. Todas as noites, ela observa o jovem Daniel. Sua curiosidade e desejo aumentam quanto mais ela percebe o quão diferente ele é da crueldade à qual ela está acostumada. Observá-lo torna-se um vício prazeroso, no entanto ela duvida que ele possa interessar-se por uma vampira assassina. Quando o rapaz se torna um alvo do misterioso grupo rival dos vampiros, ela precisa sair das sombras para protegê-lo e tenta equilibrar encontros noturnos com ele e o seu trabalho de assassina na guerra pela disputa de poder na cidade. Quanto mais tempo passam juntos, mais difícil fica de esconder sua verdadeira identidade. E enquanto a paixão aumenta, também aumentam as revelações sobre o passado sombrio de Valéria. Ela começa a desejar viver um romance em uma vida comum, porém será que a Srta. M. permitirá? Valéria conseguirá protegê-los de tantos inimigos? E será que Daniel realmente aceitará sua verdadeira natureza?';
 
 
-  imagensGif: string = '/assets/midia/imagens.gif';
+  imagensGif: string = './assets/midia/imagens.gif';
 
   constructor() {}
 
